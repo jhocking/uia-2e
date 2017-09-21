0e1ffdecd57bee6fb7c56a95ad6abcd97ebc058c
Joseph Hocking <jhocking@newarteest.com>
2017-09-20 20:06:33 -0500

    ch01

---
+++ ch01/Assets/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		Debug.Log("Hello World!");
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+}
