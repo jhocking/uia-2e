835e2e4bb866cc399a85be72a5962084b8723ec7
Joseph Hocking <jhocking@newarteest.com>
2017-09-20 20:17:15 -0500

    chapter 3 project

---
+++ ch03/Assets/Fireball.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour {
+	public float speed = 10.0f;
+	public int damage = 1;
+
+	void Update() {
+		transform.Translate(0, 0, speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		PlayerCharacter player = other.GetComponent<PlayerCharacter>();
+		if (player != null) {
+			player.Hurt(damage);
+		}
+		Destroy(this.gameObject);
+	}
+}
